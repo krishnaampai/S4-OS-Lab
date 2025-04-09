28bd1c59f1c412f8ee44e8d8310db1d612259b60
Krishnaa M Pai <krishnaampai@gmail.com>
2025-04-09 11:09:31 +0530

    Edited firstFit

--- exp04/FirstFit.c
+++ exp04/FirstFit.c
@@ -1,40 +1,69 @@
 #include <stdio.h>
-#include <string.h>
+#include<stdlib.h>
 
-void firstFit(int processSize[], int blockSize[], int m , int n){
-	int allocation[n];
-	memset(allocation,-1,sizeof(allocation));
-	
+struct Block {
+	int size;
+	int process_id;
+	struct Block* next;
+};
+
+void firstFit(int processes[],int n,struct Block* head){
 	for (int i =0;i<n;i++){
-		for (int j=0;j<m;j++){
-			if (blockSize[j] >= processSize[i]){
-				allocation[i] = j;
-				blockSize[j] = blockSize[j] - processSize[i];
+		struct Block* temp = head;
+		while(temp){
+			if(temp->process_id ==-1 && temp->size>= processes[i]){
+				temp->process_id = i+1;
+				printf("Process %d of size  %d allocated to block of size %d\n",i+1,processes[i],temp->size);
 				break;
 			}
+			temp = temp->next;
+			
+		}
+		if(!temp){
+			printf("process %d not allocated\n",i+1);
 		}
 	}
-	printf("Process no.\tProcess size\tBlock No.\n");
-	for (int i = 0;i < n;i++){
-		printf("%d\t\t%d\t\t",i+1, processSize[i]);
-		if (allocation[i]!= -1){
-			printf("%d\n", allocation[i]+1);
-		}
-		else {
-			printf("Not allocated\n");
-		}
-	}
-	printf ("Free Blocks: \n");
-	for (int i = 0; i < m; i++) {
-    	printf("Block %d: %d KB\n", i+1, blockSize[i]);
-    }
 }
 
 int main(){
-	int processSize[] = {30,500,200,220,150};
-	int blockSize[] = {30,100,50,400,350};
-	int m = sizeof(blockSize)/sizeof(blockSize[0]);
-	int n = sizeof(processSize)/sizeof(processSize[0]);
-	firstFit(processSize, blockSize, m,n);
+	struct Block* head = NULL;
+	struct Block* temp;
+	int n,m;
+	printf("Enter number of blocks: ");
+	scanf("%d",&m);
+	for (int i =0;i<m;i++){
+		
+		struct Block *newBlock = (struct Block*)malloc(sizeof(struct Block));
+		printf("Enter size of block %d:",i+1);
+		scanf("%d",&newBlock->size);
+		newBlock->process_id = -1;
+		newBlock->next = NULL;
+		if(!head){
+			head = newBlock;
+		}
+		else {
+			temp->next = newBlock;
+			
+		}
+		
+		temp = newBlock;
+		
+	}
+	printf("Enter number of processes: ");
+	scanf("%d",&n);
+	int processes[n];
+	for (int i =0;i<n;i++){
+		printf("Enter size of process %d:",i+1);
+		scanf("%d",&processes[i]);
+	}
+	firstFit(processes,n,head);
+	
+	printf ("Block\t Process\n");
+	struct Block* temp1= head;
+	while(temp1!=NULL){
+		printf("%d\t%d\n",temp1->size,temp1->process_id);
+		temp1= temp1->next;
+	}
+	
 	return 0;
 }
