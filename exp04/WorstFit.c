c8401fba5f9ed19c8deb35cd5573093f8c3d0c1a
Krishnaa M Pai <krishnaampai@gmail.com>
2025-04-09 11:30:15 +0530

    Edited worstFit

--- exp04/WorstFit.c
+++ exp04/WorstFit.c
@@ -1,46 +1,74 @@
 #include <stdio.h>
-#include <string.h>
+#include<stdlib.h>
 
-void worstFit(int processSize[], int blockSize[], int m , int n){
-	int allocation[n];
-	memset(allocation,-1,sizeof(allocation));
-	
-	for (int i =0; i < n;i++){
-		int worst = -1;
-		for (int j =0; j < m;j++){
-			if (blockSize[j] >= processSize[i]){
-				if (worst == -1 || blockSize[worst] < blockSize[j]){
-					worst = j;
+struct Block {
+	int size;
+	int process_id;
+	struct Block* next;
+};
+
+void worstFit(int processes[],int n,struct Block* head){
+	for (int i =0;i<n;i++){
+		struct Block* temp = head;
+		struct Block* worst = NULL;
+		while(temp!=NULL){
+			if(temp->process_id==-1&&temp->size >=processes[i]){
+				if(!worst||temp->size>worst->size){
+					worst = temp;
 				}
 			}
+			temp = temp->next;
 		}
-		if (worst != -1){
-			allocation[i] = worst;
-			blockSize[worst] -= processSize[i];
-		}
-	}
-	
-	printf("Process no.\tProcess size\tBlock No.\n");
-	for (int i = 0;i < n;i++){
-		printf("%d\t\t%d\t\t",i+1, processSize[i]);
-		if (allocation[i]!= -1){
-			printf("%d\n", allocation[i]+1);
+		if(worst!=NULL){
+			worst->process_id = i+1;
+			printf("Process %d of size  %d allocated to block of size %d\n",i+1,processes[i],worst->size);
+				
 		}
 		else {
-			printf("Not allocated\n");
+			printf("process %d not allocated\n",i+1);
 		}
 	}
-	printf ("Free Blocks: \n");
-	for (int i = 0; i < m; i++) {
-    	printf("Block %d: %d KB\n", i+1, blockSize[i]);
-    }
 }
 
 int main(){
-	int processSize[] = {212, 417, 112, 426};
-	int blockSize[] = {100, 500, 200, 300, 600};
-	int m = sizeof(blockSize)/sizeof(blockSize[0]);
-	int n = sizeof(processSize)/sizeof(processSize[0]);
-	worstFit(processSize, blockSize, m,n);
+	struct Block* head = NULL;
+	struct Block* temp;
+	int n,m;
+	printf("Enter number of blocks: ");
+	scanf("%d",&m);
+	for (int i =0;i<m;i++){
+		
+		struct Block *newBlock = (struct Block*)malloc(sizeof(struct Block));
+		printf("Enter size of block %d:",i+1);
+		scanf("%d",&newBlock->size);
+		newBlock->process_id = -1;
+		newBlock->next = NULL;
+		if(!head){
+			head = newBlock;
+		}
+		else {
+			temp->next = newBlock;
+			
+		}
+		
+		temp = newBlock;
+		
+	}
+	printf("Enter number of processes: ");
+	scanf("%d",&n);
+	int processes[n];
+	for (int i =0;i<n;i++){
+		printf("Enter size of process %d:",i+1);
+		scanf("%d",&processes[i]);
+	}
+	worstFit(processes,n,head);
+	
+	printf ("Block\t Process\n");
+	struct Block* temp1= head;
+	while(temp1!=NULL){
+		printf("%d\t%d\n",temp1->size,temp1->process_id);
+		temp1= temp1->next;
+	}
+	
 	return 0;
 }
